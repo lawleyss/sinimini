d3218020a3ee1bcd70d8047f40b9d069f2f388e5
Anna <annjukna@gmail.com>
2024-11-04 10:19:23 +0200

    Update Sinimini_database.sql

--- Sinimini_database.sql
+++ Sinimini_database.sql
@@ -9,14 +9,14 @@ CREATE TABLE film_and_series (
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL  -- store hashed passwords here for security
+    password VARCHAR(255) NOT NULL  
 );
 CREATE TABLE user_content_status (
     status_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     content_id INT,
     status ENUM('watched', 'watch later') NOT NULL,
-    rating DECIMAL(3, 1),  -- rating from 1.0 to 10.0, with one decimal place
+    rating DECIMAL(3, 1), 
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
     FOREIGN KEY (content_id) REFERENCES film_and_series(content_id) ON DELETE CASCADE
 );
