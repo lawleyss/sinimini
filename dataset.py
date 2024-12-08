63ff5b0f9ace692aca14b6685c6bafe5fdc1667e
uungure <151825350+uungure@users.noreply.github.com>
2024-12-08 18:14:14 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,10 +1,1 @@
-import csv
 
-with open('dataset.csv', mode='r', newline='', encoding='utf-8') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    
-    with open('dataset.txt', mode='w', encoding='utf-8') as txt_file:
-        for row in csv_reader:
-            txt_file.write('\t'.join(row) + '\n')
-
-print("CSV contents saved to output.txt with tab separation.")
